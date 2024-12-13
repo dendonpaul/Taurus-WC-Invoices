a2793eac0ca26d4352f58dc90cdae349e83ed090
dendonpaul <dendonpaul@gmail.com>
2024-12-13 11:08:58 +0530

    Removed customer added as CC and send individual email to admin and customer separately

--- includes/class.Taurus_WC_IG_Main.php
+++ includes/class.Taurus_WC_IG_Main.php
@@ -38,13 +38,16 @@ if (!class_exists('Taurus_WC_IG_Main')) {
         public function taurus_wc_ig_email_invoice($order, $pdf_file_path, $recipient_email)
         {
             $to = $recipient_email;
+            $customer_email = "";
             $subject = 'Invoice for Order #' . $order->get_order_number();
             $message = 'Please find the invoice attached for your recent order.';
 
             // Prepare headers
             $headers[] = 'Content-Type: text/html; charset=UTF-8';
             if ($this->email_options['email_customer'] === '1' && null !== $order->get_billing_email()) {
-                $headers[] = 'Cc:' . $order->get_billing_email();
+                //Add CC to email
+                // $headers[] = 'Cc:' . $order->get_billing_email();
+                $customer_email = $order->get_billing_email();
             }
 
 
@@ -59,7 +62,12 @@ if (!class_exists('Taurus_WC_IG_Main')) {
             //     //as your website to avoid being marked as spam.
             //     return 'orders@hovet.in';
             // }
+            //Send Invoice to Admin Email
             wp_mail($to, $subject, $message, $headers, $attachments);
+            //send invoice to customer
+            if (!empty($customer_email)) {
+                wp_mail($customer_email, $subject, $message, $headers, $attachments);
+            }
 
             // Optionally, delete the file after sending to avoid clutter
             unlink($pdf_file_path);
