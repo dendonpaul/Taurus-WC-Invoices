22c88822a15abff4a04d0769134b29e17d1b9e86
dendonpaul <dendonpaul@gmail.com>
2024-12-13 13:25:38 +0530

    Update: Download Invoice button fixed

--- includes/class.Taurus_WC_IG_Invoice_Download.php
+++ includes/class.Taurus_WC_IG_Invoice_Download.php
@@ -5,29 +5,44 @@ if (!class_exists('Taurus_WC_IG_Invoice_Download')) {
         function __construct()
         {
             add_action('woocommerce_admin_order_data_after_order_details', [$this, 'taurus_wcig_invoice_download_button']);
+            add_action('template_redirect', [$this, 'formHandling']);
         }
 
         public function taurus_wcig_invoice_download_button($order)
         {
+            $order_id = $order->get_id($order);
 
-            if (isset($_POST["taurus_wcig_order_id"])) {
-                echo "Dello-Henny";
-                $invoice_generator = new Taurus_WC_Invoice_Generator();
-                $pdf_file = $invoice_generator->pdf_generator($order);
-                echo $pdf_file;
-            }
 ?>
             <div class="invoice-download-button">
                 <form method="post" action="">
-                    <input type='hidden' name='taurus_wcig_order_id' id='taurus_wcig_order_id' />
-                    <input type='submit' name='taurus_wcig_download_invoice' value='Download Invoice' class="button button-primary" />
+                </form>
+            </div>
+
+            <div class="invoice-download-button">
+                <form method="post" action="">
+                    <?php wp_nonce_field('reload_with_post_data', 'custom_nonce'); ?>
+                    <input type="hidden" name="order_id" value="<?php echo esc_attr($order_id); ?>">
+                    <button type="submit" name="taurus_wcig_download_invoice" class="button button-primary">Download Invoice</button>
                 </form>
             </div>
             <style>
-                .invoice-download-button input[type=submit] {
+                .invoice-download-button form button {
                     margin-top: 20px !important;
                 }
             </style>
+
+            <?php
+
+            // Display submitted POST data (if any)
+            if (isset($_POST['taurus_wcig_download_invoice']) && isset($_POST['order_id'])) {
+                $invoice_generator = new Taurus_WC_IG_PDF_Invoice_Generator();
+                $pdf_file = $invoice_generator->pdf_generator($order);
+
+
+                header("Content-type:application/pdf");
+                header('Content-Disposition: attachment; filename=Invoice for Order: ' . $order->get_id());
+                readfile($pdf_file);
+            } ?>
 <?php
         }
     }
